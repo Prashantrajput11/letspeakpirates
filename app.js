85927f21ce4cbf8a76fe8ee149e201e2b0a28279
Prashant Tanwar <69415658+Prashantrajput11@users.noreply.github.com>
2022-01-11 21:33:05 +0530

    feat: converted code to es6

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
+
+// Reference all the necessary tags
 let btn = document.querySelector(".btn");
 let input=document.querySelector(".input-box")
 let output=document.querySelector(".output-box")
+
+
 // var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 var serverUrl="https://api.funtranslations.com/translate/pirate.json"
 
-function getUrl(text)
-{
-return serverUrl + "?" +"text="  +text
-}
-// getUrl("i am crazy")
-
-btn.addEventListener("click",function(){
-
-console.log("working");
-var inputText=input.value 
+// construct url by appending the user's text
+let getUrl = text => serverUrl + "?" + "text=" + text
 
 
-fetch(getUrl(inputText))
-.then(response=>response.json())
-.then(json=>{var outputText=json.contents.translated
-    output.value=outputText});
-})
+    // Get the data (fetched by url) on click on the button.
+    let getData =  () =>{
 
+        let inputText = input.value
 
+        // Fetch data
+        fetch(getUrl(inputText))
+        .then(
+            response => response.json() // converts the response in readable format
+        )
+        .then(
+            json => {
+                let outputText = json.contents.translated
+                output.value = outputText
+            }
+        )
+    }
 
+    // Handler function
+    btn.addEventListener("click", getData)
